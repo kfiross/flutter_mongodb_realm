4723415043f6ea9782831a033499b3e4e15ce56c
kfiross <7c034cfd9ee8g>
2021-02-06 19:20:05 +0200

    updated ios plugin configuration to work

--- ios/Classes/FlutterMongoStitchPlugin.m
+++ ios/Classes/FlutterMongoStitchPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterMongoStitchPlugin.h"
-#if __has_include(<flutter_mongo_stitch/flutter_mongo_stitch-Swift.h>)
-#import <flutter_mongo_stitch/flutter_mongo_stitch-Swift.h>
+#if __has_include(<flutter_mongodb_realm/flutter_mongodb_realm-Swift.h>)
+#import <flutter_mongodb_realm/flutter_mongodb_realm-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_mongo_stitch-Swift.h"
+#import "flutter_mongodb_realm-Swift.h"
 #endif
 
 @implementation FlutterMongoStitchPlugin
